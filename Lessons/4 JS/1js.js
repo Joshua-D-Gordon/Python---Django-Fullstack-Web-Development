6e83f6ca513e4b372e9ed199aa4ce64e96b799cb
Joshua gordon <jodogo9897@gmail.com>
2023-08-07 15:19:02 +0300

    started JS 	new file:   Lessons/4 JS/1index.html 	new file:   Lessons/4 JS/1js.js 	new file:   Lessons/4 JS/2index.html 	new file:   Lessons/4 JS/2js.js

---
+++ Lessons/4 JS/1js.js
@@ -0,0 +1,5 @@
+alert("welcome to your bank");
+
+var deposite = prompt("how much to deposite?");
+alert("you have deposited: " + deposite);
+console.log("you are a cool person");
---
+++ Lessons/4 JS/2js.js
@@ -0,0 +1,22 @@
+var hot = false;
+var temp = 50
+
+if( temp > 80){
+    console.log("hot");
+}else if(temp <= 80 && temp >= 50){
+    console.log("avg");
+}else if(temp < 50 && temp >= 32){
+    console.log("cold");
+}else{
+    console.log("frezzing");
+}
+
+var x = 0;
+while(x < 10){
+    console.log(x);
+    x++;
+}
+
+for(var i = 0; i< 10; i++){
+    console.log(i);
+}
