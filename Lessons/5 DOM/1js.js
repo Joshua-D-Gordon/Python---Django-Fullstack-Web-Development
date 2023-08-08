67f9b1bad7270b90b81f8cead98467c9ea474b8f
Joshua gordon <jodogo9897@gmail.com>
2023-08-08 15:46:02 +0300

    changing h1 colors with js 	new file:   Lessons/5 DOM/1index.html 	new file:   Lessons/5 DOM/1js.js

---
+++ Lessons/5 DOM/1js.js
@@ -0,0 +1,21 @@
+
+var header = document.querySelector("h1")
+
+header.style.color = 'red'
+
+function randomcolor(){
+    var letters = "0123456789ABCDEF";
+    var color = '#';
+    for(var i = 0; i< 6; i++){
+        color+= letters[Math.floor(Math.random()*16)];
+    }
+
+    return color;
+}
+
+function changeheadercolor(){
+    colorinput = randomcolor()
+    header.style.color = colorinput;
+}
+
+setInterval("changeheadercolor()", 500);
