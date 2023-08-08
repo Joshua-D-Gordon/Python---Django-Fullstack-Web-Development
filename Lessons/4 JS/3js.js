e83e245b0eaea296dfed647399a6582592342905
Joshua gordon <jodogo9897@gmail.com>
2023-08-08 15:08:23 +0300

    Started javascript objects 	new file:   Lessons/4 JS/3index.html 	new file:   Lessons/4 JS/3js.js 	new file:   Lessons/4 JS/4index.html 	new file:   Lessons/4 JS/4js.js

---
+++ Lessons/4 JS/3js.js
@@ -0,0 +1,20 @@
+
+function name(par1, par2){
+    //do code
+}
+
+function hello(){
+    console.log("hello world");
+}
+
+function helloyou(name){
+    console.log("hello "+name);
+}
+
+function addNum(a, b){
+    console.log(a+b);
+}
+
+function helloSomeone(name="Frankie"){
+    console.log("hello "+name);
+}
---
+++ Lessons/4 JS/4js.js
@@ -0,0 +1,22 @@
+var coutrys = ["USA", "GERMANY", "CHINA"];
+
+function print(string){
+    console.log(string);
+}
+
+function printall(arr){
+    arr.array.forEach(print);
+}
+
+
+//objects
+
+var car = {
+    make: "tyota",
+    year: 1990,
+    model: "camry",
+    carAlert: function(){
+        alert("wev got a car here")
+    }
+};
+
