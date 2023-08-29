fdea95b4ad699383a902aecee1828ccddb4775e1
Joshua gordon <jodogo9897@gmail.com>
2023-08-29 14:13:09 +0300

    added exercises python 	modified:   Lessons/7 python/3_functions.py 	new file:   Lessons/7 python/Part6_Exercise_Review.py 	new file:   Lessons/7 python/Part9_Functions_Exercises.py

--- Lessons/7 python/3_functions.py
+++ Lessons/7 python/3_functions.py
@@ -6,3 +6,23 @@ if __name__ == "__main__":
     b = 5
 
     print(add(a,b))
+
+    # filter function
+    mylist = [1,2,3,4,5,6,7,8]
+
+    def even_bool(num):
+        return num%2 == 0
+    
+    evens = filter(even_bool, mylist)
+
+    print(list(evens))
+
+    #lambda num: num%2 == 1
+
+    odds = filter(lambda num: num%2 == 1, mylist)
+    print(list(odds))
+
+    st = 'hello'
+    st.upper()
+    st.lower()
+    print(st.split('#'))
